ad0f8c0f2e98057faa0b5acf3e52765c5c6c62c5
Daniel Häggström <danielhaggstrom@hotmail.se>
2022-02-28 18:09:09 +0100

    api in progress on services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,48 +1,20 @@
 import Cards from "./Cards";
+import { useState, useEffect } from "react";
 
 const Services = () => {
-  // Just a temporary object with card information for now,
-  // later I'll fetch data from an API and use that.
-  let cardInfo = [
-    {
-      title: "Card 1",
-      content: "This is card 1",
-    },
-    {
-      title: "Card 2",
-      content: "This is card 2",
-    },
-    {
-      title: "Card 3",
-      content: "This is card 3",
-    },
-    {
-      title: "Card 4",
-      content: "This is card 4",
-    },
-    {
-      title: "Card 5",
-      content: "This is card 5",
-    },
-    {
-      title: "Card 6",
-      content: "This is card 6",
-    },
-    {
-      title: "Future update",
-      content: "Button to create new card",
-    },
-    {
-      title: "Charmander",
-      content: "Fire",
-    },
-  ];
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=9&offset=0";
+  const [pokemon, setPokemon] = useState([]);
 
+  useEffect(async () => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setPokemon(data.results);
+  }, []);
   return (
     <section className="section">
       <div className="card-container">
-        {cardInfo.map((card) => (
-          <Cards title={card.title} content={card.content} />
+        {pokemon.map((poke, index) => (
+          <Cards key={index} title={poke.name} content="wip" />
         ))}
       </div>
     </section>
